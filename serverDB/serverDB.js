5fb0fbe1aa69a1939e7661354f4d9f33ed3ea93a
Dmytro Tyshchenko <kwazar2008@ukr.net>
2019-03-12 12:06:52 +0200

    GET and POST

--- serverDB/serverDB.js
+++ serverDB/serverDB.js
@@ -17,15 +17,74 @@ const server = http.createServer((request, response) => {
         });
         const result = JSON.stringify(themes);
         response.end(result);
+    } else if (request.method === "GET" && request.url.startsWith("/db/text")) {
+        if (myURL.query.category !== undefined && myURL.query.theme !== undefined) {
+            const category = myURL.query.category;
+            const theme = myURL.query.theme;
+            const str = fs.readFileSync("texts.json", "utf8");
+            const texts = JSON.parse(str);
+            if (Object.keys(texts).some(key => key === category)) {
+                if (Object.keys(texts[category]).some(key => key === theme)) {
+                    const index = Math.trunc(texts[category][theme].length * Math.random());
+                    // = index;
+                    const text = JSON.stringify(texts[category][theme][index]);
+                    response.statusCode = 200;
+                    response.setHeader("Content-Type", "application/json");
+                    response.end(text);
+                } else {
+                    response.statusCode = 404;
+                    response.setHeader("Content-Type", "text/plain");
+                    response.end(`404 Text with theme: ${theme} not found!`);
+                }
+            } else {
+                response.statusCode = 404;
+                response.setHeader("Content-Type", "text/plain");
+                response.end(`404 Text with category: ${category} not found!`);
+            }
+        } else {
+            response.statusCode = 404;
+            response.setHeader("Content-Type", "text/plain");
+            response.end(`404 Incorrect parameters`);
+        }
+    } else if (request.method === "POST" && request.url.startsWith("/db/add/text")) {
+        if (request.headers["Content-Type"] !== "application/json") {
+            response.statusCode = 404;
+            response.setHeader("Content-Type", "text/plain");
+            response.end(`404 Incorrect data recived`);
+        }
+        request.setEncoding("utf8");
+        let data = "";
+        request.on("data", chunk => { data += chunk; });
+        request.on("end", () => {
+            let text;
+            try {
+                text = JSON.parse(data);
+            } catch (err) {
+                console.error(err.message);
+            }
+            if (text !== undefined && text[category] !== undefined && text[theme] !== undefined && text[body] !== undefined) {
+                const str = fs.readFileSync("texts.json", "utf8");
+                const texts = JSON.parse(str);
+                texts[category][theme].push(text[body]);
+                fs.writeFileSync("texts.json", JSON.stringify(texts));
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "text/plain");
+                response.end("OK");
+            } else {
+                response.statusCode = 404;
+                response.setHeader("Content-Type", "text/plain");
+                response.end(`404 Incorrect data recived`);
+            }
+        });
     } else if (request.method === "GET" && request.url.startsWith("/db/user")) {
-        response.statusCode = 200;
         if (myURL.query.login !== undefined) {
             const login = myURL.query.login;
-            response.setHeader("Content-Type", "application/json");
             const str = fs.readFileSync("users.json", "utf8");
             const users = JSON.parse(str);
             if (Object.keys(users).some(key => key === login)) {
                 const user = JSON.stringify(users.login);
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "application/json");
                 response.end(user);
             } else {
                 response.statusCode = 404;
@@ -37,9 +96,43 @@ const server = http.createServer((request, response) => {
             response.setHeader("Content-Type", "text/plain");
             response.end(`404 Incorrect parameters`);
         }
-    } /* else if (request.method === "POST") {
-        if (myURL.query.login !== undefined)
-    } */ else {
+    } else if (request.method === "POST" && request.url.startsWith("/db/add/user")) {
+        if (request.headers["Content-Type"] !== "application/json") {
+            response.statusCode = 404;
+            response.setHeader("Content-Type", "text/plain");
+            response.end(`404 Incorrect data recived`);
+        }
+        request.setEncoding("utf8");
+        let data = "";
+        request.on("data", chunk => { data += chunk; });
+        request.on("end", () => {
+            let user;
+            try {
+                user = JSON.parse(data);
+            } catch (err) {
+                console.error(err.message);
+            }
+            if (user !== undefined) {
+                const str = fs.readFileSync("users.json", "utf8");
+                const users = JSON.parse(str);
+                if (Object.keys(users).every(key => key !== user.login)) {
+                    users[user.login] = user;
+                    fs.writeFileSync("users.json", JSON.stringify(users));
+                    response.statusCode = 200;
+                    response.setHeader("Content-Type", "text/plain");
+                    response.end("OK");
+                } else {
+                    response.statusCode = 404;
+                    response.setHeader("Content-Type", "text/plain");
+                    response.end(`404 User with login: ${user.login} alredy exist!`);
+                }
+            } else {
+                response.statusCode = 404;
+                response.setHeader("Content-Type", "text/plain");
+                response.end(`404 Incorrect data recived`);
+            }
+        });
+    } else {
         response.statusCode = 404;
         response.setHeader("Content-Type", "text/plain");
         response.end("404 Resourse not found!");
