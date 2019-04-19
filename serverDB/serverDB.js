119665363deafc2a6713c80ffe1b1056b4d8c403
Dmytro Tyshchenko <kwazar2008@ukr.net>
2019-04-19 18:36:56 +0300

    install dotenv

--- serverDB/serverDB.js
+++ serverDB/serverDB.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const url = require("url");
-const fs = require("fs");
+// const fs = require("fs");
+require('dotenv').config();
 const db = require("./db");
 
 const PORT = process.env.PORT || 3000;
