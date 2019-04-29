73f73e1cb2b7107041d54c950257013d5e1b0149
Dmytro Tyshchenko <kwazar2008@ukr.net>
2019-04-29 22:07:44 +0300

    production

--- serverDB/db/index.js
+++ serverDB/db/index.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
