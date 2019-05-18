52684b80a045ea7d012de59a1cf98cfa588dd3d2
Dmytro Tyshchenko <kwazar2008@ukr.net>
2019-05-18 15:24:50 +0300

    update settings connection to database

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString, max: 20 });
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
